e76a1a6f8f00e147fd65cd6fc2b3558d2116af5f
AJ Barea <ajbareaa@gmail.com>
2024-06-04 10:54:06 -0700

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,26 @@
+function skillsMember() {
+  var member = {
+    name: "Skills",
+    type: "skills",
+    skills: [
+      {
+        name: "Java",
+        level: 4,
+      },
+      {
+        name: "Python",
+        level: 3,
+      },
+      {
+        name: "JavaScript",
+        level: 2,
+      },
+      {
+        name: "SQL",
+        level: 3,
+      },
+    ],
+  };
+
+  return member;
+}
