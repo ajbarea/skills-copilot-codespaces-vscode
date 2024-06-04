aef3e83586fce4c1f156210d366e4143e90f793f
AJ Barea <ajbareaa@gmail.com>
2024-06-04 11:15:02 -0700

    chore: Refactor comments.js

--- comments.js
+++ comments.js
@@ -1,34 +1,12 @@
-//Create a webserver
-var http = require("http");
-var fs = require("fs");
-var path = require("path");
-var url = require("url");
-var comments = [];
-var mime = require("mime");
-var server = http.createServer(function (req, res) {
-  var urlObj = url.parse(req.url, true);
-  var pathname = urlObj.pathname;
-  if (pathname == "/") {
-    res.setHeader("Content-Type", "text/html;charset=utf-8");
-    fs.createReadStream("./comment.html").pipe(res);
-  } else if (pathname == "/comment") {
-    var comment = urlObj.query;
-    comments.push(comment);
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    res.end();
-  } else if (pathname == "/getComments") {
-    res.end(JSON.stringify(comments));
-  } else {
-    fs.access("." + pathname, function (err) {
-      if (!err) {
-        res.setHeader("Content-Type", mime.lookup(pathname) + ";charset=utf-8");
-        fs.createReadStream("." + pathname).pipe(res);
-      } else {
-        res.statusCode = 404;
-        res.end("Not Found");
-      }
-    });
-  }
+//Create a web server
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
-server.listen(8080);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
